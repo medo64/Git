0cdffb84cc09ca5077306ff60a2ba61382653a8a
Josip Medved <jmedved@jmedved.com>
2015-11-03 18:19:01 -0800

    Different ways to see history.

--- settings.sh
+++ settings.sh
@@ -4,6 +4,10 @@ git config --global system.autocrlf false
 # Don't mark diffs with ^M
 git config --global core.whitespace cr-at-eol
 
+
+# git changed - show files changed with commit
+git config --global alias.changed 'log --all --decorate --graph --pretty=short --name-status'
+
 # git ls - shows one-line status for all modified files
 git config --global alias.ls 'status --short --branch'
 
@@ -11,7 +15,10 @@ git config --global alias.ls 'status --short --branch'
 git config --global alias.last 'log -1'
 
 # git history - shows decorated log
-git config --global alias.history 'log --all --decorate --graph --pretty=format:"%C(auto)%h %s %d%C(reset)"'
+git config --global alias.hist 'log --all --decorate --graph --pretty=format:"%C(auto)%h %s %d%C(reset)"'
+
+# git history - shows decorated log (longer form)
+git config --global alias.history 'log --all --decorate --graph --pretty=format:"%C(auto)%h %s %C(green)[%an, %ar%C(auto)] %d%C(reset)"'
 
 # git pending - shows what is pending push toward origin
 git config --global alias.pending 'log master ^origin/master'
